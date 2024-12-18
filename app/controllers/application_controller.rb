856d66527002e86573f6cdd4de706f0c373c6dd3
OmarMWarraich <earthling986@gmail.com>
2024-12-18 23:11:45 +0500

    feat: Internationalization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   protect_from_forgery with: :null_session, if: -> { request.format.json? }
 
+  around_action :switch_locale
+
   def user_session_id
     if authenticated?
       Session.first.user_id
@@ -12,5 +14,17 @@ class ApplicationController < ActionController::Base
 
   def current_user
     User.find_by(id: user_session_id)
+  end
+
+  def switch_locale(&action)
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{locale}'"
+    I18n.with_locale(locale, &action)
+  end
+
+  private
+  def extract_locale_from_accept_language_header
+    request.env["HTTP_ACCEPT_LANGUAGE"].scan(/^[a-z]{2}/).first
   end
 end
