057319e9a67be56957d05543110255a26595799b
OmarMWarraich <earthling986@gmail.com>
2024-12-26 19:18:09 +0500

    feat: In Stock Email Notifications

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,16 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true
   validates :inventory_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  after_update_commit :notify_subscribers, if: :back_in_stock?
+
+  def back_in_stock?
+    inventory_count_previously_was.zero? && inventory_count.positive?
+  end
+
+  def notify_subscribers
+    subscribers.each do |subscriber|
+      ProductMailer.with(product: self, subscriber: subscriber).in_stock.deliver_later
+    end
+  end
 end
