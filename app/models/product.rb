2a990cd4eae39bdc3e61f698a14f54b22887f181
OmarMWarraich <earthling986@gmail.com>
2024-12-26 17:32:57 +0500

    feat: Instock Notifications

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true
+  validates :inventory_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_18_163510) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_26_122648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_18_163510) do
     t.float "price", default: 0.0, null: false
     t.text "description"
     t.bigint "user_id", null: false
+    t.integer "inventory_count"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
